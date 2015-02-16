f00b23f2c31d8ca0bd4e522a779c1e49d2d373e2
rohans90 <rohans@umich.edu>
2015-02-16 11:56:42 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# this function works like a class, it creates a list that contains 4 member
+# functions: set, get, setinverse, getinverse. It uses <<- assignment operator so that 
+#these Internal variables are not exposed to the outside environment
 
 makeCacheMatrix <- function(x = matrix()) {
-
+m <- NULL
+set <- function(y)
+{
+x <<- y
+m <<- NULL
+}
+get <- function() x
+setInverse <- function(inverse) m <<- inverse
+getInverse <- function() m
+list (set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x,...){
+##Returns a matrix that is the inverse of 'x'
+m <- x$getInverse()
+if(!is.null(m)){
+message("getting cached data")
+return(m)
 }
+data <- x$get()
+m <- solve(data,...)
+x$setInverse(m)
+m
+}
+#Test
+#generate a random non singular matrix
+test <- matrix(runif(9,1,100),3,3)
+#generate the makeCacheMatrix object with the matrix
+testCached <- makeCacheMatrix(test)
+
